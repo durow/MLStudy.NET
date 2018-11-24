83a4d87a58b5812d7e1996678ca80bf9349d309e
durow <durow@163.com>
2018-11-24 19:41:48 +0800

    add and test MaxPooling layer

---
+++ MLStudy.Tests/Deep/Layers/PoolingLayerTests.cs
@@ -0,0 +1,217 @@
+﻿using MLStudy.Deep;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MLStudy.Tests.Deep.Layers
+{
+    public class PoolingLayerTests
+    {
+        [Fact]
+        public void ForwardBackwardTest()
+        {
+            var data = new double[]
+            {
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5
+            };
+            var input = new Tensor(data, 1, 1, 4, 4);
+            var pooling = new MaxPooling(2);
+            pooling.PrepareTrain(input);
+            var forward = pooling.Forward(input);
+            var expected = new Tensor(new double[]
+            {
+                4,6,
+                8,9
+            }, 1, 1, 2, 2);
+
+            Assert.Equal(expected, forward);
+
+            var error = new Tensor(new double[] 
+            {
+                0.5, 0.7,
+                -0.3, 1.2
+            }, 1, 1, 2, 2);
+            var backward = pooling.Backward(error);
+            var backExpected = new Tensor(new double[]
+            {
+                0,0,0,0,
+                0.5,0,0.7,0,
+                -0.3,0,0,0,
+                0,0,1.2,0
+            }, 1, 1, 4, 4);
+            Assert.Equal(backExpected, backward);
+        }
+
+        [Fact]
+        public void ForwardTest2()
+        {
+            var data = new double[]
+            {
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+            };
+            var input = new Tensor(data, 1, 3, 4, 4);
+            var pooling = new MaxPooling(2);
+            pooling.PrepareTrain(input);
+            var actual = pooling.Forward(input);
+            var expected = new Tensor(new double[]
+            {
+                4,6,
+                8,9,
+
+                4,6,
+                8,9,
+
+                4,6,
+                8,9,
+            }, 1, 3, 2, 2);
+
+            Assert.Equal(expected, actual);
+
+            var error = new Tensor(new double[]
+            {
+                0.5, 0.7,
+                -0.3, 1.2,
+
+                0.5, 0.7,
+                -0.3, 1.2,
+
+                0.5, 0.7,
+                -0.3, 1.2,
+            }, 1, 3, 2, 2);
+            var backward = pooling.Backward(error);
+            var backExpected = new Tensor(new double[]
+            {
+                0,0,0,0,
+                0.5,0,0.7,0,
+                -0.3,0,0,0,
+                0,0,1.2,0,
+
+                0,0,0,0,
+                0.5,0,0.7,0,
+                -0.3,0,0,0,
+                0,0,1.2,0,
+
+                0,0,0,0,
+                0.5,0,0.7,0,
+                -0.3,0,0,0,
+                0,0,1.2,0,
+            }, 1, 3, 4, 4);
+            Assert.Equal(backExpected, backward);
+        }
+
+        [Fact]
+        public void ForwardTest3()
+        {
+            var data = new double[]
+            {
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+
+                3,2,1,5,
+                4,2,6,1,
+                8,2,6,3,
+                6,3,9,5,
+            };
+            var input = new Tensor(data, 2, 3, 4, 4);
+            var pooling = new MaxPooling(1, 3, 1, 3);
+            pooling.PrepareTrain(input);
+            var actual = pooling.Forward(input);
+            var expected = new Tensor(new double[]
+            {
+                3,6,8,9,
+                3,6,8,9,
+                3,6,8,9,
+                3,6,8,9,
+                3,6,8,9,
+                3,6,8,9,
+            }, 2, 3, 4, 1);
+
+            Assert.Equal(expected, actual);
+
+            var error = new Tensor(new double[]
+            {
+                0.8,0.7,-1.5,0.4,
+                0.8,0.7,-1.5,0.4,
+                0.8,0.7,-1.5,0.4,
+                0.8,0.7,-1.5,0.4,
+                0.8,0.7,-1.5,0.4,
+                0.8,0.7,-1.5,0.4,
+            }, 2, 3, 4, 1);
+            var backward = pooling.Backward(error);
+
+            var backData = new double[]
+            {
+                0.8,0,0,0,
+                0,0,0.7,0,
+                -1.5,0,0,0,
+                0,0,0.4,0,
+
+                0.8,0,0,0,
+                0,0,0.7,0,
+                -1.5,0,0,0,
+                0,0,0.4,0,
+
+                0.8,0,0,0,
+                0,0,0.7,0,
+                -1.5,0,0,0,
+                0,0,0.4,0,
+
+                0.8,0,0,0,
+                0,0,0.7,0,
+                -1.5,0,0,0,
+                0,0,0.4,0,
+
+                0.8,0,0,0,
+                0,0,0.7,0,
+                -1.5,0,0,0,
+                0,0,0.4,0,
+
+                0.8,0,0,0,
+                0,0,0.7,0,
+                -1.5,0,0,0,
+                0,0,0.4,0,
+            };
+            var backExpected = new Tensor(backData, 2, 3, 4, 4);
+            Assert.Equal(backExpected, backward);
+        }
+    }
+}
--- MLStudy/Deep/Layers/MaxPooling.cs
+++ MLStudy/Deep/Layers/MaxPooling.cs
@@ -1,10 +1,150 @@
-﻿using System;
+﻿using MLStudy.Abstraction;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MLStudy.Deep
 {
-    public sealed class MaxPooling
+    public sealed class MaxPooling : ILayer
     {
+        public string Name { get; set; }
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+        public int RowStride { get; private set; }
+        public int ColumnStride { get; private set; }
+        public Tensor ForwardOutput { get; private set; }
+        public Tensor BackwardOutput { get; private set; }
+
+        private int samples;
+        private int channels;
+        private int outRows;
+        private int outColumns;
+        private Tensor maxPositions;
+
+        public MaxPooling(int sizeAndStride)
+            : this(sizeAndStride, sizeAndStride, sizeAndStride, sizeAndStride)
+        { }
+
+        public MaxPooling(int square, int stride)
+            : this(square, square, stride, stride)
+        { }
+
+        public MaxPooling(int rows, int columns, int rowStride, int columnStride)
+        {
+            Rows = rows;
+            Columns = columns;
+            RowStride = rowStride;
+            ColumnStride = columnStride;
+        }
+
+        public Tensor PrepareTrain(Tensor input)
+        {
+            PreparePredict(input);
+
+            BackwardOutput = input.GetSameShape();
+            maxPositions = new Tensor(samples, channels, outRows * outColumns, 2);
+
+            return ForwardOutput;
+        }
+
+        public Tensor PreparePredict(Tensor input)
+        {
+            if (input.Rank != 4)
+                throw new Exception("MaxPooling layer input rank must be 4!");
+
+            outRows = (input.shape[2] - Rows) / RowStride + 1;
+            outColumns = (input.shape[3] - Columns) / ColumnStride + 1;
+            samples = input.shape[0];
+            channels = input.shape[1];
+            ForwardOutput = new Tensor(samples, channels, outRows, outColumns);
+
+            return ForwardOutput;
+        }
+
+        public Tensor Forward(Tensor input)
+        {
+            Parallel.For(0, ForwardOutput.shape[0], sampleIndex =>
+            {
+                Parallel.For(0, ForwardOutput.shape[1], channel =>
+                {
+                    PoolingChannel(input, sampleIndex, channel);
+                });
+            });
+            return ForwardOutput;
+        }
+
+        public Tensor Backward(Tensor error)
+        {
+            BackwardOutput.Clear();
+
+            Parallel.For(0, ForwardOutput.shape[0], sampleIndex =>
+            {
+                Parallel.For(0, ForwardOutput.shape[1], channel =>
+                {
+                    ErrorBP(error, sampleIndex, channel);
+                });
+            });
+            return BackwardOutput;
+        }
+
+        public ILayer CreateSame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PoolingChannel(Tensor input, int sample, int channel)
+        {
+            var maxIndex = 0;
+            for (int row = 0; row < outRows; row++)
+            {
+                for (int col = 0; col < outColumns; col++)
+                {
+                    var inputRow = row * RowStride;
+                    var inputColumn = col * ColumnStride;
+                    var max = input[sample, channel, inputRow, inputColumn];
+                    var maxRow = inputRow;
+                    var maxColumn = inputColumn;
+
+                    for (int i = 0; i < Rows; i++)
+                    {
+                        for (int j = 0; j < Columns; j++)
+                        {
+                            if (input[sample, channel, inputRow + i, inputColumn + j] > max)
+                            {
+                                max = input[sample, channel, inputRow + i, inputColumn + j];
+                                maxRow = inputRow + i;
+                                maxColumn = inputColumn + j;
+                            }
+                        }
+                    }
+
+                    ForwardOutput[sample, channel, row, col] = max;
+                    if (maxPositions != null)
+                    {
+                        maxPositions[sample, channel, maxIndex, 0] = maxRow;
+                        maxPositions[sample, channel, maxIndex, 1] = maxColumn;
+                        maxIndex++;
+                    }
+                }
+            }
+        }
+
+        private void ErrorBP(Tensor error, int sample, int channel)
+        {
+            var maxIndex = 0;
+            for (int i = 0; i < outRows; i++)
+            {
+                for (int j = 0; j < outColumns; j++)
+                {
+                    var e = error[sample, channel, i, j];
+                    var maxRow = maxPositions[sample, channel, maxIndex, 0];
+                    var maxCol = maxPositions[sample, channel, maxIndex, 1];
+                    BackwardOutput[sample, channel, (int)maxRow, (int)maxCol] += e;
+
+                    maxIndex++;
+                }
+            }
+        }
     }
 }
