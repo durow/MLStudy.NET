b2583136ca17e1d98c2ac3b578f7612ae2abd41d
durow <durow@163.com>
2018-11-27 18:18:24 +0800

    add FlattenLayer to make 4 rank tensor from CovLayer to 2 ranke tensor into FC layers

---
+++ MLStudy/Deep/FlattenLayer.cs
@@ -0,0 +1,50 @@
+﻿using MLStudy.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLStudy.Deep
+{
+    public class FlattenLayer : ILayer
+    {
+        public string Name { get; set; }
+        private int[] lastInputShape;
+
+        public Tensor Backward(Tensor error)
+        {
+            return GetUnFlatten(error);
+        }
+
+        public ILayer CreateSame()
+        {
+            return new FlattenLayer();
+        }
+
+        public Tensor Forward(Tensor input)
+        {
+            return GetFlatten(input);
+        }
+
+        public Tensor PreparePredict(Tensor input)
+        {
+            return GetFlatten(input);
+        }
+
+        public Tensor PrepareTrain(Tensor input)
+        {
+            lastInputShape = input.shape;
+            return GetFlatten(input);
+        }
+
+        private Tensor GetFlatten(Tensor input)
+        {
+            var d1 = input.shape[0];
+            return input.Reshape(d1, input.ElementCount / d1);
+        }
+
+        private Tensor GetUnFlatten(Tensor error)
+        {
+            return error.Reshape(lastInputShape);
+        }
+    }
+}
